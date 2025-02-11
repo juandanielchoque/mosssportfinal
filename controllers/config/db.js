793667457a2d5a124b27885492c4f9ffb35794f4
juanchoque <juan.choque.m@tecsup.edu.pe>
2025-02-11 15:43:32 -0500

    comimt15

--- controllers/config/db.js
+++ controllers/config/db.js
@@ -1,12 +1,21 @@
-const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'moss_sportv2',
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS, // Asegúrate de que se esté usando
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
-const dbPromise = db.promise();
+db.getConnection()
+    .then(conn => {
+        console.log('✅ Conexión exitosa a la base de datos');
+        conn.release();
+    })
+    .catch(err => {
+        console.error('❌ Error conectando a la BD:', err.message);
+    });
 
-module.exports = dbPromise;
+module.exports = db;
