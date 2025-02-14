c599e717b5813e448ba3d9fb4878d9c4c07c319d
juanchoque <juan.choque.m@tecsup.edu.pe>
2025-02-14 02:06:23 -0500

    commit2.1

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ const db = require('./config/db');
 
 // Configuración de CORS
 app.use(cors({
-  origin: 'https://moss-sport-frontend-final.vercel.app/', // Reemplaza con la URL de tu frontend en Vercel
+  origin: 'https://moss-sport-frontend-final.vercel.app', // Reemplaza con la URL de tu frontend en Vercel
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'Backend activo 🚀' });
+});
+
 
 // Middleware
 app.use(bodyParser.json());
